20d6158c07b84bda22c41368524e3efabe4f9ce5
annamanucharyan272 <77779024+annamanucharyan272@users.noreply.github.com>
2023-02-02 18:52:50 +0400

    Add files via upload

---
+++ Filters_9plots.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import signal
+from numpy import asarray
+from numpy import savetxt
+
+# Load the signals data
+data = np.loadtxt('ID_AV Movement 1.txt')
+t = data[:, 0]
+signals = data[:, 1:]
+#print(t)
+#print(signals)
+
+# Define the sampling frequency
+fs = 1 / (t[1] - t[0])
+
+# Design the Bandpass filter
+b1, a1 = signal.butter(4, 10/(fs/2), 'highpass') #butterworth
+b2, a2 = signal.butter(4, [15, 35], 'bandpass', fs=fs) #bandpass
+
+# Apply the filters to each signal
+filtered_signals = np.zeros_like(signals)
+for i in range(signals.shape[1]):
+    filtered_signals[:, i] = signal.filtfilt(b1, a1, signals[:, i]) #butterworth
+    filtered_signals[:, i] = signal.lfilter(b2, a2, filtered_signals[:, i]) #bandpass
+    filtered_signals[:, i] = np.abs(filtered_signals[:, i])
+    data_min = min(filtered_signals[:,i])
+    data_max = max(filtered_signals[:, i])
+    filtered_signals[:, i] = (filtered_signals[:, i] - data_min)/(data_max - data_min)
+    plt.plot(t, filtered_signals, color = 'g')
+    plt.savefig("AH1_" + str(i+1) + " filtered")
+ 
+# Plot the original and filtered signals
+plt.xlabel('Time [s]')
+plt.ylabel('Amplitude [mV]')
+plt.figure()
+#plt.plot(t, signals, color = 'b')
+#plt.plot(t, filtered_signals, color = 'g')
+
+
+#save to csv file
+df = pd.DataFrame(filtered_signals, index = None)
+df.insert(0, "seconds", t)
+df.to_csv("AV1_filtered.csv", index = False)
+#data = asarray(filtered_signals)
